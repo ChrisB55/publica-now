7a3468b81921383bb7a498c7eade6c2a12bf70f0
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-10-04 15:26:18 -0400

    Separate out modal code

---
+++ assets/js/vanilla.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Currently this app.js manages only the modal functionality, which is done with vanilla js. 
+var modal = document.getElementById('projectModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Event Listeners
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open modal and Close modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
